1903cf307137f190c61f8b70de259734ca7601f9
Thaonnor <31021055+Thaonnor@users.noreply.github.com>
2022-08-27 21:49:26 -0400

    soft implemented delete functionality

--- db/articleQueries.js
+++ db/articleQueries.js
@@ -14,34 +14,47 @@ const getArticleById = (req, res, next) => {
     const id = req.params.id;
 
     pool.query('SELECT * from article WHERE id = $1', [id], (err, results) => {
-        if(err){
+        if (err) {
             res.status(500).send(err);
         }
 
         res.status(200).json(results.rows[0]);
-    })
+    });
 };
 
 const getArticlesPage = (req, res, next) => {
     const page = req.params.page;
     const limit = 10;
-    const offset = (page * limit) - limit;
-    
+    const offset = page * limit - limit;
 
-    pool.query('SELECT * FROM article LIMIT $1 OFFSET $2', [limit, offset], (err, results) => {
-        if(err) {
-            res.status(500).send(err);
+    pool.query(
+        'SELECT * FROM article LIMIT $1 OFFSET $2',
+        [limit, offset],
+        (err, results) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+
+            res.status(200).json(results.rows);
         }
-
-        res.status(200).json(results.rows);
-    })
-}
+    );
+};
 
 const createArticle = (req, res, next) => {};
 
 const updateArticle = (req, res, next) => {};
 
-const deleteArticle = (req, res, next) => {};
+const deleteArticle = (req, res, next) => {
+    const id = req.params.id;
+
+    pool.query('DELETE FROM article WHERE id = $1', [id], (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+
+        res.status(204).send();
+    });
+};
 
 module.exports = {
     getArticles,
